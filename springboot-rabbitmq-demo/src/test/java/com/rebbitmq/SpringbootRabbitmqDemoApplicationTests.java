866b8f36d7f0b9a0cdfc1891b18f0c9e0142ccd9
. <vishwanath.a.vishwanath@capgemini.com>
2025-01-28 14:47:50 +0530

    my first commit

---
+++ springboot-rabbitmq-demo/src/test/java/com/rebbitmq/SpringbootRabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
